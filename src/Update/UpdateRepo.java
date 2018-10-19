a5091aa8842aa03fb705abcdaa1a4ff4264f41de
Domenico Verde <domenico.verde@aol.com>
2018-10-19 11:25:44 +0200

    + Setup Update system

--- src/Update/UpdateRepo.java
+++ src/Update/UpdateRepo.java
@@ -14,6 +14,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 
 public class UpdateRepo {
 
@@ -78,9 +79,11 @@ public class UpdateRepo {
             ui.getStatus().setText(ui.getSettings().getResourceBundle().getString("errorReadingUpdate"));
          }
       } catch (IOException e) {
-         e.printStackTrace();
-
          ui.getStatus().setText(ui.getSettings().getResourceBundle().getString("errorReadingUpdate"));
+
+         if (e instanceof UnknownHostException) {
+            ui.getStatus().setText(ui.getSettings().getResourceBundle().getString("checkConnection"));
+         }
       }
    }
 
