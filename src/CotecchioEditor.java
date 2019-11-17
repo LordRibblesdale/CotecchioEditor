31ff028267c1c3d4a5061794ed720c8d7910bf49
Green <domenico.verde@aol.com>
2019-11-17 22:09:36 +0100

    + Prepare for newer release

--- src/CotecchioEditor.java
+++ src/CotecchioEditor.java
@@ -1,13 +1,7 @@
 import Interface.UserController;
 
-import javax.swing.*;
-
 public class CotecchioEditor implements FileManager.Path {
    public static void main(String[] args) {
-      SwingUtilities.invokeLater(new Runnable(){
-         public void run() {
-            new UserController();
-         }
-      });
+      new UserController();
    }
 }
